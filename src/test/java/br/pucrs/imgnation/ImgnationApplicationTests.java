8b04a0ab0eb711604728ca83dab6e7322d0b13c5
Mario Specht <ms.specht@gmail.com>
2019-11-26 15:59:36 -0300

    Final FCK Desenv

--- src/test/java/br/pucrs/imgnation/ImgnationApplicationTests.java
+++ src/test/java/br/pucrs/imgnation/ImgnationApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ImgnationApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
